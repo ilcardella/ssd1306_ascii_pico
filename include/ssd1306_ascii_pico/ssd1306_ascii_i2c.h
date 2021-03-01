e790b9f2b87a2fb18cdedb862e41b096e99c239b
Alberto Cardellini <albe.carde@gmail.com>
2021-03-01 00:15:41 +0000

    Made i2c bus configurable

--- include/ssd1306_ascii_pico/ssd1306_ascii_i2c.h
+++ include/ssd1306_ascii_pico/ssd1306_ascii_i2c.h
@@ -2,50 +2,48 @@
 
 #include "SSD1306Ascii.h"
 
-#include "hardware/i2c.h"
-#include "pico/binary_info.h"
-
-#define I2C_PORT i2c0
+#include <hardware/i2c.h>
+#include <pico/binary_info.h>
 
 class I2CBus
 {
   public:
-    I2CBus()
+    I2CBus(i2c_inst_t *port, const unsigned char &sda_pin, const unsigned char &scl_pin,
+           const unsigned long speed)
+        : port(port)
     {
-        // This example will use I2C0 on GPIO4 (SDA) and GPIO5 (SCL)
-        i2c_init(I2C_PORT, 100 * 1000);
-        gpio_set_function(4, GPIO_FUNC_I2C);
-        gpio_set_function(5, GPIO_FUNC_I2C);
-        gpio_pull_up(4);
-        gpio_pull_up(5);
+        i2c_init(port, speed);
+        gpio_set_function(sda_pin, GPIO_FUNC_I2C);
+        gpio_set_function(scl_pin, GPIO_FUNC_I2C);
+        gpio_pull_up(sda_pin);
+        gpio_pull_up(scl_pin);
         // Make the I2C pins available to picotool
-        bi_decl(bi_2pins_with_func(4, 5, GPIO_FUNC_I2C));
+        bi_decl(bi_2pins_with_func(sda_pin, scl_pin, GPIO_FUNC_I2C));
     }
-
-    // TODO another parametrized constructor would be useful
 
     void setClock(const long &speed)
     {
-        // TODO
+        i2c_set_baudrate(port, speed);
     }
 
     void endTransmission()
     {
-        // TODO
+        // Nothing to do
     }
 
     void beginTransmission(const uint8_t &addr)
     {
-        m_i2cAddr = addr;
+        address = addr;
     }
 
     void write(const uint8_t &c)
     {
-        i2c_write_blocking(I2C_PORT, m_i2cAddr, &c, 1, false);
+        i2c_write_blocking(port, address, &c, 1, false);
     }
 
   private:
-    uint8_t m_i2cAddr;
+    uint8_t address;
+    i2c_inst_t *port;
 };
 
 /**
