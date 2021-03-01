e9706384d1139e967262d90fb660bc58ffc57a11
Alberto Cardellini <albe.carde@gmail.com>
2021-03-01 00:19:16 +0000

    Made i2c bus configurable (#1)

--- example/example_main.cpp
+++ example/example_main.cpp
@@ -1,9 +1,11 @@
+#include <hardware/i2c.h>
 #include <pico/stdlib.h>
+
 #include <ssd1306_ascii_pico/ssd1306_ascii_i2c.h>
 
 int main()
 {
-    I2CBus bus = I2CBus();
+    I2CBus bus = I2CBus(i2c0, 4, 5, 400000L);
     SSD1306AsciiI2C display = SSD1306AsciiI2C(bus);
 
     display.begin(&Adafruit128x64, 0x3C);
